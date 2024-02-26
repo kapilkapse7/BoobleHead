35cb2bcc071efa25c90ba9a11e5eb98d18c8f65a
Snail Mail <kapilkapse007@gmail.com>
2024-02-26 15:42:50 +0530

    add admin section to manage products

---
+++ admin/src/Assets/data.js
@@ -0,0 +1,34 @@
+import p1_img from './product_1.webp'
+import p2_img from './product_2.webp'
+import p3_img from './product_3.webp'
+import p4_img from './product_4.webp'
+
+let data_product = [
+  {
+    id:1,
+    name:"Harry Potter Bobblehead",
+    image:p1_img,
+    new_price:499.00,
+    old_price:699.00,
+  },
+  {id:2,
+    name:"Batman Bobblehead",
+    image:p2_img,
+    new_price:449.00,
+    old_price:699.00,
+  },
+  {id:3,
+    name:"Itachi Uchiha Bobblehead",
+    image:p3_img,
+    new_price:60.00,
+    old_price:100.50,
+  },
+  {id:4,
+    name:"Krishna Bobblehead",
+    image:p4_img,
+    new_price:100.00,
+    old_price:150.00,
+  },
+];
+
+export default data_product;
---
+++ admin/src/Components/ListProduct/ListProduct.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Listproduct = () => {
+  return (
+    <div><h1>Listproduct</h1></div>
+  )
+}
+
+export default Listproduct
